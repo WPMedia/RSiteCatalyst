03f9ddd96630f26341ed4cfb4c3e4d721fbb9513
walshj <walshj@users.noreply.github.com>
2017-07-31 10:12:40 -0400

    Better fix

--- R/QueueRanked.R
+++ R/QueueRanked.R
@@ -60,9 +60,9 @@
 #' @export
 
 QueueRanked <- function(reportsuite.id, date.from, date.to, metrics, elements,
-                        top=0,start=0,selected=c(), search=c(),search.type='or',
-                        segment.id='', segments=NULL, segment.inline='', classification=c(),data.current=FALSE, 
-                        expedite=FALSE,interval.seconds=5,max.attempts=120,validate=TRUE) {
+                        top=10,start=1,selected=c(), search=c(),search.type='or',
+                        segment.id='', segments=NULL, segment.inline='', classification=c(),data.current=FALSE,
+                        expedite=FALSE,interval.seconds=5,max.attempts=120,validate=TRUE,enqueueOnly=FALSE) {
 
 
   # build JSON description
